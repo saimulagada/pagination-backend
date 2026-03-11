4158a916f3160bb31d11a03e7ac86a27b937b5ae
saiMulagada <sai.star47@gmail.com>
2026-03-11 22:18:43 +0530

    added varaibles for db

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
-const mysql= require("mysql2");
+require("dotenv").config();
+const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password:"313224",
-    database: "test"
-})
-module.exports=pool.promise()
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+module.exports = pool.promise();
